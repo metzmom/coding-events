dfd0d548585c08d5c13338daa4de93d7a2361377
Margie Metz <metzmom8@gmail.com>
2021-10-13 11:06:16 -0500

    end of video three render a form

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,14 +13,15 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+   private static List<String> events = new ArrayList<>();
 
    @GetMapping
    public String displayAllEvents(Model model) {
-      List<String> events = new ArrayList<>();
-      events.add("Wedding");
-      events.add("Engagement Party");
-      events.add("Bachelor Party");
-      events.add("Reception");
+//      List<String> events = new ArrayList<>();
+//      events.add("Wedding");
+//      events.add("Engagement Party");
+//      events.add("Bachelor Party");
+//      events.add("Reception");
       model.addAttribute("events", events);
       return "events/index";
    }
@@ -26,6 +29,16 @@ public class EventController {
    @GetMapping("create")
    public String displayCreateEventForm() {
       return "events/create";
+   }
+   //lives at /events/create
+   @PostMapping("create")
+   public String createEvent(@RequestParam String eventName){
+      events.add(eventName);
+      return "redirect:";//do not need /events after : as the path is still in events Controller
+
+
+
+
    }
 }
 
