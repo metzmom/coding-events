23cfd0927a5ec46cd009faf0a0e5d45c69847f6c
Margie Metz <metzmom8@gmail.com>
2021-10-14 13:44:09 -0500

    video 2.3 add ID for model unique ids

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,6 +1,14 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
+    //video 2.4 assigning a unique identifier
+    private int id;//private id field.Private so no one can edit
+    private static int nextId = 1;//static counter  belongs to class,
+    // not any single object. so every new object will be upped by one and be unique
+    // see public constructor
+
     private String name;
     private String description;
 
@@ -9,6 +17,12 @@ public class Event {
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;//video 2.3  constructor for id and increments id
+        nextId++;
+    }
+    public int getId() {//no setter...do not want anyone to change.
+        return id;
+        // No get/set for nextId since private and we want no one to change it
     }
 
     public String getName() {
@@ -26,9 +40,23 @@ public class Event {
     public void setDescription(String description) {
         this.description = description;
     }
-
-    @Override
-    public String toString() {
+   @Override
+   public String toString() {
         return  name ;
+    }
+    //need hashcode whenever you crate own Java objects:
+    //use auto generator by right clicking
+
+    @Override// video 2.3
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override//video 2.3
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
